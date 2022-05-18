ff334f96333e3d0b59b671e94f7942164ee0d3f4
pprae <pprae@innovativesol.com>
2022-05-18 12:37:48 +0100

    terraform files

--- Django-Terraform/new.py
+++
@@ -1,2 +0,0 @@
-n = raw_input("Enter your name?\n")
-# print("Hello, %s!" % n)
--- Helm-Charts/helm_release_web.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "web" {
-    name = "my-local-web-chart"
-    chart = "django-k8s"
-    repository = "/Users/mac/dev/django-k8s/Django-Terraform/webchart/django-k8s"
-    max_history = 3
-    wait = true
-    reset_values = true
-
-}
